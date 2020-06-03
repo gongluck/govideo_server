36e6eaeaee47046ea5417c2b9e48b60cb61cc3b3
gongluck <1039994845@qq.com>
2020-06-03 18:34:25 +0800

    静态文件服务

--- main.go
+++ main.go
@@ -2,13 +2,14 @@
  * @Author: gongluck
  * @Date: 2020-06-03 11:34:06
  * @Last Modified by: gongluck
- * @Last Modified time: 2020-06-03 15:23:38
+ * @Last Modified time: 2020-06-03 17:57:43
  */
 
 package main
 
 import (
 	"govideo_server/handler"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -27,6 +28,8 @@ func main() {
 
 		api.POST("/postvideo", handler.PostVideo)
 	}
+
+	r.StaticFS("/videos", http.Dir("./videos"))
 
 	r.Run() // 监听并在 0.0.0.0:8080 上启动服务
 }
